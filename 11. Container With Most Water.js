0d5b7f1ec45add81e7a67fffc96759aff685c35b
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-10-06 22:29:26 +0600

    better 11

--- 11. Container With Most Water.js
+++ 11. Container With Most Water.js
@@ -1,15 +1,32 @@
-/**
- * @param {number[]} height
- * @return {number}
- */
-var maxArea = function(height) {
+// Time Complexity: O(n^2)
+
+const maxArea = function (height) {
     let area, maxArea = 0;
-    for(let i = 0; i < height.length; i++) {
-        for(let j = i+1; j < height.length; j++) {
+    for (let i = 0; i < height.length; i++) {
+        for (let j = i + 1; j < height.length; j++) {
             area = Math.min(height[i], height[j]) * Math.abs(i - j);
-            if(area > maxArea) maxArea = area;
+            if (area > maxArea) maxArea = area;
         }
     }
-    
+
+    return maxArea;
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+function maxArea(height) {
+    let start = 0;
+    let end = height.length - 1;
+    let maxArea = 0;
+
+    while (start < end) { // if end < start that means all the elements are already compared
+        const currentArea = Math.min(height[start], height[end]) * (end - start); //height gottabe the lower among two edges otherwise container can't be made, width = end - start
+        maxArea = Math.max(currentArea, maxArea);
+
+        if (height[start] < height[end]) start++;
+        else end--;
+    }
+
     return maxArea;
 };
