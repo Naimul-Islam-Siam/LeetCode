a439d71c5c9cb39a15bbc8f79cb4dfa8ffc77703
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2023-01-24 23:34:28 +0600

    FizzBuzz

---
+++ 412. FizzBuzz.js
@@ -0,0 +1,35 @@
+/*
+   Given an integer n, return a string array answer (1-indexed) where:
+
+      answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+      answer[i] == "Fizz" if i is divisible by 3.
+      answer[i] == "Buzz" if i is divisible by 5.
+      answer[i] == i (as a string) if none of the above conditions are true.
+
+   Input: n = 3
+   Output: ["1","2","Fizz"]
+   Example 2:
+
+   Input: n = 5
+   Output: ["1","2","Fizz","4","Buzz"]
+*/
+
+// Time Complexity: O(n)
+// Space Complexity: O(n) considering output otherwise O(1)
+function fizzBuzz(n) {
+   let answer = [];
+
+   for (let i = 1; i <= n; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+         answer[i - 1] = "FizzBuzz";
+      } else if (i % 3 == 0) {
+         answer[i - 1] = "Fizz";
+      } else if (i % 5 == 0) {
+         answer[i - 1] = "Buzz";
+      } else {
+         answer[i - 1] = i.toString();
+      }
+   }
+
+   return answer;
+};
