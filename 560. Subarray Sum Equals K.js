d453a4fb4fa6bbb07daf4e4accc63525c56a50a2
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2023-02-20 00:12:17 +0600

    Subarray sum equals K

--- 560. Subarray Sum Equals K.js
+++ 560. Subarray Sum Equals K.js
@@ -1,3 +1,18 @@
+/*
+   Given an array of integers nums and an integer k, 
+   return the total number of subarrays whose sum equals to k.
+
+   A subarray is a contiguous non-empty sequence of elements within an array.
+
+   Input: nums = [1,1,1], k = 2
+   Output: 2
+
+   Input: nums = [1,2,3], k = 3
+   Output: 2
+
+   https://youtu.be/fFVZt-6sgyo
+*/
+
 // Time Complexity: O(n^2)
 // Space Complexity: O(1)
 function subarraySum(nums, k) {
@@ -17,6 +32,36 @@ function subarraySum(nums, k) {
          if (sum == k) {
             count++;
          }
+      }
+   }
+
+   return count;
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+function subarraySum(nums, k) {
+   /*
+      we will be using Map instead of set because there can be negative numbers in nums array
+      so multiple subarray can produce the same sum value [1] = [1, -1, 1]
+      we will initialize with 0 as count value 1 in Map
+   */
+   let prefixCount = new Map([[0, 1]]); // initialize with 0: 1
+   let sum = 0;
+   let count = 0;
+
+   for (let i = 0; i < nums.length; i++) {
+      sum = sum + nums[i]; // prefix sum
+      nums[i] = sum;
+
+      if (prefixCount.has(sum - k)) {
+         count = count + prefixCount.get(sum - k);
+      }
+
+      if (!prefixCount.has(sum)) {
+         prefixCount.set(sum, 1);
+      } else {
+         prefixCount.set(sum, prefixCount.get(sum) + 1);
       }
    }
 
